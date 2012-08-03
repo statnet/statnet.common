336c4bba4d13d9fd55ecf02760d6cec0d21cf4a4
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-03 06:07:22 +0000

    Added and documented citation utilities that produce package citation information from a template.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet.common/trunk@11601 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ R/cite.utilities.R
@@ -0,0 +1,34 @@
+statnet.cite.head <- function(pkg)
+  citHeader(
+    paste("`",pkg,"` is part of the Statnet suite of packages.  ",
+          "If you are using the `",pkg,"` package for research that will be published, ",
+          "we request that you acknowledge this by citing the following.\n\n",
+          'For BibTeX format, use toBibtex(citation("',pkg,'")).',
+          sep="")
+    )
+
+statnet.cite.foot <- function(pkg)
+  citFooter("We have invested a lot of time and effort in creating the",
+            "Statnet suite of packages for use by other researchers.",
+            "Please cite it in all papers where it is used.")
+
+statnet.cite.pkg <- function(pkg){
+  
+  desc <- packageDescription(pkg)
+
+  projhomepage <- "http://www.statnet.org"
+    
+  auts <- eval(parse(text=desc$`Authors@R`))
+  auts <- auts[sapply(auts, function(aut) "aut" %in% aut$role)]
+  
+  bibentry("Manual",
+           author = auts,
+           title = desc$Title,
+           year         = substr(desc$Date,1,4),
+           note         = paste("Version ", desc$Version,
+             ".  Project home page at \\url{", projhomepage, "}", 
+             sep=""),
+           address      = "Seattle, WA",
+           url          = paste("CRAN.R-project.org/package=",desc$Package,sep="")
+           )
+}
