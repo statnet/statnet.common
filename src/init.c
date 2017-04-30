582e66b0f07ba1f40c60dad96655f15d98cebc3f
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-04-30 05:19:29 +0000

    Added native routine registration.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet.common/trunk@14877 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ src/init.c
@@ -0,0 +1,30 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP log_sum_exp_wrapper(SEXP, SEXP);
+extern SEXP logspace_wmean_wrapper(SEXP, SEXP);
+extern SEXP logspace_wmean2_wrapper(SEXP, SEXP);
+extern SEXP logspace_wmeans_wrapper(SEXP, SEXP);
+extern SEXP sweep2m(SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"log_sum_exp_wrapper",     (DL_FUNC) &log_sum_exp_wrapper,     2},
+    {"logspace_wmean_wrapper",  (DL_FUNC) &logspace_wmean_wrapper,  2},
+    {"logspace_wmean2_wrapper", (DL_FUNC) &logspace_wmean2_wrapper, 2},
+    {"logspace_wmeans_wrapper", (DL_FUNC) &logspace_wmeans_wrapper, 2},
+    {"sweep2m",                 (DL_FUNC) &sweep2m,                 2},
+    {NULL, NULL, 0}
+};
+
+void R_init_statnet_common(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
