5074ea38ea11b48ef3a53f4e631747560b8859cf
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-07-10 21:14:05 +1000

    Added reexports from rle, for smoother transitions.

---
+++ R/rle_reexport.R
@@ -0,0 +1,135 @@
+#' @name rle-reexport
+#'
+#' @title Functions reexported from `rle`
+#'
+#' They will no longer be reexported after the next release.
+#'
+#' @import rle
+NULL
+
+## #' @export
+## c.rle <- rle::c.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## `!.rle` <- rle::`!.rle`
+
+#' @rdname rle-reexport
+#' @export
+binop.rle <- rle::binop.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## `|.rle` <- rle::`|.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `&.rle` <- rle::`&.rle`
+
+#' @rdname rle-reexport
+#' @export
+compress <- rle::compress
+
+## #' @rdname rle-reexport
+## #' @export
+## compress.rle <- rle::compress.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## any.rle <- rle::any.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## all.rle <- rle::all.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## `*.rle` <- rle::`*.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `/.rle` <- rle::`/.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `-.rle` <- rle::`-.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `+.rle` <- rle::`+.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `^.rle` <- rle::`^.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `%%.rle` <- rle::`%%.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `%/%.rle` <- rle::`%/%.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `==.rle` <- rle::`==.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `>.rle` <- rle::`>.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `<.rle` <- rle::`<.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `!=.rle` <- rle::`!=.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `<=.rle` <- rle::`<=.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## `>=.rle` <- rle::`>=.rle`
+
+## #' @rdname rle-reexport
+## #' @export
+## sum.rle <- rle::sum.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## mean.rle <- rle::mean.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## length.rle <- rle::length.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## is.na.rle <- rle::is.na.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## rep.rle <- rle::rep.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## as.rle <- rle::as.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## as.rle.rle <- rle::as.rle.rle
+
+## #' @rdname rle-reexport
+## #' @export
+## as.rle.default <- rle::as.rle.default
+
+## #' @rdname rle-reexport
+## #' @export
+## str.rle <- rle::str.rle
+
+#' @rdname rle-reexport
+#' @export
+compact.rle <- rle::compact.rle
