5f3fd9395edbe52ed2df4839189e069e7dc15d91
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-05-29 00:08:32 +1000

    Updated copyright headers.

--- R/misc.utilities.R
+++ R/misc.utilities.R
@@ -1,11 +1,11 @@
-#  File R/misc.utilities.R in package statnet.common, part of the
-#  Statnet suite of packages for network analysis, https://statnet.org .
+#  File R/misc.utilities.R in package statnet.common, part of the Statnet suite
+#  of packages for network analysis, https://statnet.org .
 #
-#  This software is distributed under the GPL-3 license.  It is free,
-#  open source, and has the attribution requirements (GPL Section 7) at
+#  This software is distributed under the GPL-3 license.  It is free, open
+#  source, and has the attribution requirements (GPL Section 7) at
 #  https://statnet.org/attribution .
 #
-#  Copyright 2007-2024 Statnet Commons
+#  Copyright 2007-2025 Statnet Commons
 ################################################################################
 #' reorder vector v into order determined by matching the names of its elements
 #' to a vector of names
--- inst/templates/snctrl.R
+++ inst/templates/snctrl.R
@@ -1,11 +1,11 @@
-#  File inst/templates/snctrl.R in package statnet.common, part of the
-#  Statnet suite of packages for network analysis, https://statnet.org .
+#  File inst/templates/snctrl.R in package statnet.common, part of the Statnet
+#  suite of packages for network analysis, https://statnet.org .
 #
-#  This software is distributed under the GPL-3 license.  It is free,
-#  open source, and has the attribution requirements (GPL Section 7) at
+#  This software is distributed under the GPL-3 license.  It is free, open
+#  source, and has the attribution requirements (GPL Section 7) at
 #  https://statnet.org/attribution .
 #
-#  Copyright 2007-2024 Statnet Commons
+#  Copyright 2007-2025 Statnet Commons
 ################################################################################
 # This document provides a template for exporting a package's
 # control.* functions to be visible to snctrl() and providing a
