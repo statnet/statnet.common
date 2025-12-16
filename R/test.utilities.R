7d4852ce13dd33d9b8fad3ad991770a584dc6dbc
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-12-16 22:28:42 +1100

    Updated copyright headers.

--- R/test.utilities.R
+++ R/test.utilities.R
@@ -1,3 +1,12 @@
+#  File R/test.utilities.R in package statnet.common, part of the Statnet suite
+#  of packages for network analysis, https://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free, open
+#  source, and has the attribution requirements (GPL Section 7) at
+#  https://statnet.org/attribution .
+#
+#  Copyright 2007-2025 Statnet Commons
+################################################################################
 
 #' Skip a test if not called via `R CMD` [`check`]
 #'
